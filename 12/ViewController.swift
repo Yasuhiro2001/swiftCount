b075fadb35111c72e784b9e81f9d61b8964f9c50
古田泰大 <yasuhiro2001@icloud.com>
2015-04-12 14:02:45 +0900

    初回のコミット

--- 12/ViewController.swift
+++ 12/ViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
+    
+var number:Int=0
+@IBOutlet var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction  func  plus() {
+        number=number+1
+        label.text=String(number)
+        
+    }
+    
+    @IBAction func minus(){
+        number=number-1
+        label.text=String(number)
+    }
 }
+
+
+
 
